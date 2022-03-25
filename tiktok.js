5e4a7dea9c1ee952437617493d2999f49ba656f8
Gabinks <47298993+Gabinks@users.noreply.github.com>
2022-03-25 01:41:13 +0100

    Update tiktok.js

--- tiktok.js
+++ tiktok.js
@@ -40,7 +40,7 @@ const sync = async (userID) => {
         }
         db.set('cache', newPostsSorted.map((post) => post.id))
     }catch (error){
-        console.error('TIKTOK' + error)
+        console.error('1TIKTOK ' + error)
     }
 }
 
@@ -50,7 +50,7 @@ client.on('ready', async () => {
         sync(userID)
         setInterval(() => sync(userID), 120 * 1000)
     }catch (error){
-        console.log('TIKTOK' + error)
+        console.log('2TIKTOK ' + error)
     }
 })
 
